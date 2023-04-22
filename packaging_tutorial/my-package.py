2c8df4c4daff0e99ce25a4074e7975d37d467833
Abdullah Al Zubaer <23630737+abdullahalzubaer@users.noreply.github.com>
2023-04-23 00:30:06 +0200

    first-last-commit

---
+++ packaging_tutorial/my-package.py
@@ -0,0 +1,6 @@
+from example_package_abdullahalzubaer_22april2023 import example
+example.add_one(2)
+
+
+
+
---
+++ packaging_tutorial/src/example_package_abdullahalzubaer_22april2023/example.py
@@ -0,0 +1,9 @@
+""" A simple python project packaging tutorial from here
+https://packaging.python.org/en/latest/tutorials/packaging-projects/"""
+
+
+def add_one(number):
+    """
+    Toy function
+    """
+    return number + 1
